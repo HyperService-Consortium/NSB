c34c41a03f1383dd3b4531ed877784577c806650
Myriad-Dreamin <camiyoru@gmail.com>
2019-04-24 22:31:45 +0800

    merkmap

--- merkmap/merkmap.go
+++ merkmap/merkmap.go
@@ -2,6 +2,7 @@ package merkmap
 
 import (
 	"fmt"
+	"encoding/hex"
 	"github.com/Myriad-Dreamin/go-mpt"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/Myriad-Dreamin/NSB/merkmap/MerkMapError"
@@ -17,29 +18,34 @@ func (mp *MerkMap) location(key []byte) []byte {
 	return []byte("")
 }
 
-func NewMerkMapFromDB(db *leveldb.DB, slot interface{}) (mp *MerkMap, err error) {
-	switch slot := slot.(type) {
+func NewMerkMapFromDB(db *leveldb.DB, rootHash trie.Hash, slot interface{}) (mp *MerkMap, err error) {
+	mp = &MerkMap{merk: trie.NewTrie(rootHash, db)}
+	switch ori_slot := slot.(type) {
 	case string:
 		//hexstring
-		fmt.Println(slot)
-		return nil, nil
+		mp.slot, err = hex.DecodeString(ori_slot)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("string", ori_slot, mp.slot)
+		return
 	case []byte:
 		// trans into [32]byte
-		fmt.Println(slot)
+		fmt.Println("byte", ori_slot)
 		return nil, nil
 	case [32]byte:
-		fmt.Println(slot)
+		fmt.Println("[32]byte", ori_slot)
 		return nil, nil
 	default:
 		return nil, MerkMapError.UnrecognizedType
 	}
 }
 
-func NewMerkMap(dbDir string, slot interface{}) (mp *MerkMap, err error) {
+func NewMerkMap(dbDir string, rootHash trie.Hash, slot interface{}) (mp *MerkMap, err error) {
 	var db *leveldb.DB
 	db, err = leveldb.OpenFile(dbDir, nil)
 	if err != nil {
 		return
 	}
-	return NewMerkMapFromDB(db, slot)
+	return NewMerkMapFromDB(db, rootHash, slot)
 }
--- merkmap/merkmap_test.go
+++ merkmap/merkmap_test.go
@@ -1,10 +1,16 @@
 package merkmap
 
 import (
-	_ "fmt"
+	"fmt"
 	"testing"
+	"github.com/Myriad-Dreamin/go-mpt"
 )
 
 func TestMerkMap(t *testing.T) {
-
+	merkmap, err := NewMerkMap("./testdb", trie.HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"), "00")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	fmt.Println(merkmap)
 }
