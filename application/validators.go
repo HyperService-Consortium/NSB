712643c4a215eb1f42528f387387772188d856f5
Myriad-Dreamin <camiyoru@gmail.com>
2019-04-25 15:50:20 +0800

    Update validators.go

--- application/validators.go
+++ application/validators.go
@@ -10,17 +10,17 @@ import (
 )
 
 func (nsb *NSBApplication) Validators() (validators []types.ValidatorUpdate) {
-	itr := nsb.state.db.Iterator(nil, nil)
-	for ; itr.Valid(); itr.Next() {
-		if isValidatorTx(itr.Key()) {
-			validator := new(types.ValidatorUpdate)
-			err := types.ReadMessage(bytes.NewBuffer(itr.Value()), validator)
-			if err != nil {
-				panic(err)
-			}
-			validators = append(validators, *validator)
-		}
-	}
+	// itr := nsb.state.db.Iterator(nil, nil)
+	// for ; itr.Valid(); itr.Next() {
+	// 	if isValidatorTx(itr.Key()) {
+	// 		validator := new(types.ValidatorUpdate)
+	// 		err := types.ReadMessage(bytes.NewBuffer(itr.Value()), validator)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		validators = append(validators, *validator)
+	// 	}
+	// }
 	return
 }
 
@@ -29,9 +29,9 @@ func MakeValSetChangeTx(pubkey types.PubKey, power int64) []byte {
 }
 
 func (nsb *NSBApplication) execValidatorTx(tx []byte) types.ResponseDeliverTx {
-	// tx = tx[len(ValidatorSetChangePrefix):]
+	tx = tx[len(ValidatorSetChangePrefix):]
 
-	//get the pubkey and power
+	get the pubkey and power
 	pubKeyAndPower := strings.Split(string(tx), "/")
 	if len(pubKeyAndPower) != 2 {
 		return types.ResponseDeliverTx{
@@ -61,5 +61,27 @@ func (nsb *NSBApplication) execValidatorTx(tx []byte) types.ResponseDeliverTx {
 }
 
 func (nsb *NSBApplication) updateValidator(v types.ValidatorUpdate) types.ResponseDeliverTx {
+	// key := []byte("val:" + string(v.PubKey.Data))
+	// if v.Power == 0 {
+	// 	// remove validator
+	// 	if !nsb.state.db.Has(key) {
+	// 		return types.ResponseDeliverTx{
+	// 			Code: code.CodeTypeUnauthorized,
+	// 			Log:  fmt.Sprintf("Cannot remove non-existent validator %X", key)}
+	// 	}
+	// 	nsb.state.db.Delete(key)
+	// } else {
+	// 	// add or update validator
+	// 	value := bytes.NewBuffer(make([]byte, 0))
+	// 	if err := types.WriteMessage(&v, value); err != nil {
+	// 		return types.ResponseDeliverTx{
+	// 			Code: code.CodeTypeEncodingError,
+	// 			Log:  fmt.Sprintf("Error encoding validator: %v", err)}
+	// 	}
+	// 	nsb.state.db.Set(key, value.Bytes())
+	// }
+
+	// // we only update the changes array if we successfully updated the tree
+	// nsb.ValUpdates = append(nsb.ValUpdates, v)
 	return types.ResponseDeliverTx{Code: uint32(CodeOK)}
 }
