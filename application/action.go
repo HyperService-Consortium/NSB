425b999c897cfd0375950ac5aff5db113a28128d
Myriad-Dreamin <camiyoru@gmail.com>
2019-04-25 17:32:32 +0800

    addr

---
+++ application/action.go
@@ -0,0 +1,31 @@
+package nsb
+
+func (nsb *NSBApplication) addAction() (validators []types.ValidatorUpdate) {
+	// itr := nsb.state.db.Iterator(nil, nil)
+	// for ; itr.Valid(); itr.Next() {
+	// 	if isValidatorTx(itr.Key()) {
+	// 		validator := new(types.ValidatorUpdate)
+	// 		err := types.ReadMessage(bytes.NewBuffer(itr.Value()), validator)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		validators = append(validators, *validator)
+	// 	}
+	// }
+	return
+}
+
+func (nsb *NSBApplication) getAction() (validators []types.ValidatorUpdate) {
+	// itr := nsb.state.db.Iterator(nil, nil)
+	// for ; itr.Valid(); itr.Next() {
+	// 	if isValidatorTx(itr.Key()) {
+	// 		validator := new(types.ValidatorUpdate)
+	// 		err := types.ReadMessage(bytes.NewBuffer(itr.Value()), validator)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		validators = append(validators, *validator)
+	// 	}
+	// }
+	return
+}
--- application/isc/isc.go
+++ application/isc/isc.go
@@ -1,5 +1,27 @@
 package isc
 
-func NewISC() {
-	//
+import (
+	"math/rand"
+	"time"
+	"github.com/Myriad-Dreamin/go-mpt"
+)
+
+
+type Address []byte
+
+func NewISCAddress() (addr Address) {
+	rand.Seed(time.Now().UnixNano())
+	addr = Keccak256(
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+		[]byte(string(rand.Intn(1 << 31))),
+	)
+
+	return 
 }
