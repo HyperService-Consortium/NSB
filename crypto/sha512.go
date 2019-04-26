11d0393d1f6495bbc5fa8c6097689ea268fb9a77
Myriad-Dreamin <camiyoru@gmail.com>
2019-04-26 20:37:26 +0800

    crypto

--- crypto/sha512.go
+++ crypto/sha512.go
@@ -1,14 +1,13 @@
 package crypto
 
 import (
-	"crypto/sha256"
+	"crypto/sha512"
 )
 
-func Sha256(data ...[]byte) []byte {
-	d := sha256.New()
+func Sha512(data ...[]byte) []byte {
+	d := sha512.New()
 	for _, b := range data {
 		d.Write(b)
 	}
 	return d.Sum(nil)
 }
-
