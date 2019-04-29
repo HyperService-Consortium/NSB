ee9411497856022546a4e0aa86f95c22b9100115
Myriad-Dreamin <camiyoru@gmail.com>
2019-04-29 20:51:06 +0800

    wallet

--- account/wallet.go
+++ account/wallet.go
@@ -2,65 +2,136 @@ package account
 
 
 import (
-	"github.com/syndtr/goleveldb/leveldb"
-	"encoding/hex"
 	"errors"
-	eddsa "golang.org/x/crypto/ed25519"
+	"github.com/syndtr/goleveldb/leveldb"
+	"encoding/json"
+	"encoding/hex"
+	"bytes"
+)
+
+var (
+	prikeyHeader = []byte("Private Key: ")
+	pubkeyHeader = []byte("Public Key: ")
+	newlineByte = byte('\n')
 )
 
 type Wallet struct {
 	db *leveldb.DB
-	Acc *Account
+	name string
+	Acc []*Account `json:"accs"`
 }
 
-func NewWallet(db *leveldb.DB, acc *Account) *Wallet {
+func NewWallet(db *leveldb.DB, name string) *Wallet {
 	return &Wallet{
 		db: db,
-		Acc: acc,
+		name: name,
 	}
 }
 
-func ReadWallet(db *leveldb.DB, pubkey interface{}) (*Wallet, error) {
+func ReadWallet(db *leveldb.DB, name string) (*Wallet, error) {
 	if db == nil {
 		return nil, errors.New("nil database pointer")
 	}
-	switch pubkey := pubkey.(type) {
-	case string:
-		pri, err := hex.DecodeString(pubkey)
-		if err != nil {
-			return nil, err
-		}
-		pri, err = db.Get(pri, nil)
-		if err != nil {
-			return nil, err
-		}
-		return &Wallet{
-			db: db,
-			Acc: ReadAccount(pri),
-		}, nil
-	case []byte:
-		pri, err := db.Get(pubkey, nil)
-		if err != nil {
-			return nil, err
-		}
-		return &Wallet{
-			db: db,
-			Acc: ReadAccount(pri),
-		}, nil
-	case eddsa.PublicKey:
-		pri, err := db.Get([]byte(pubkey), nil)
-		if err != nil {
-			return nil, err
-		}
-		return &Wallet{
-			db: db,
-			Acc: ReadAccount(pri),
-		}, nil
-	default:
-		return nil, errors.New("invalid public key type")
+	
+	bt, err := db.Get([]byte(name), nil)
+	if err != nil {
+		return nil, err
 	}
+	var wlt Wallet
+	err = json.Unmarshal(bt, &wlt)
+	if err != nil {
+		return nil, err
+	}
+	wlt.db = db
+	wlt.name = name
+	return &wlt, nil
+	// switch pubkey := pubkey.(type) {
+	// case string:
+	// 	pri, err := hex.DecodeString(pubkey)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	pri, err = db.Get(pri, nil)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	return &Wallet{
+	// 		db: db,
+	// 		Acc: ReadAccount(pri),
+	// 	}, nil
+	// case []byte:
+	// 	pri, err := db.Get(pubkey, nil)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	return &Wallet{
+	// 		db: db,
+	// 		Acc: ReadAccount(pri),
+	// 	}, nil
+	// case eddsa.PublicKey:
+	// 	pri, err := db.Get([]byte(pubkey), nil)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	// 	return &Wallet{
+	// 		db: db,
+	// 		Acc: ReadAccount(pri),
+	// 	}, nil
+	// default:
+	// 	return nil, errors.New("invalid public key type")
+	// }
 }
 
-func (wt *Wallet) Save() error {
-	return wt.db.Put(wt.Acc.PublicKey, wt.Acc.PrivateKey, nil)
+
+func (wlt *Wallet) AppendAccount(acc *Account) {
+	wlt.Acc = append(wlt.Acc, acc)
+}
+
+
+func (wlt *Wallet) String() string {
+	if len(wlt.Acc) == 0 {
+		return "<empty>"
+	}
+	var ret bytes.Buffer
+	var str string
+	for _, acc := range wlt.Acc {
+		
+		ret.Write(prikeyHeader)
+		str = hex.EncodeToString(acc.PrivateKey)
+		ret.WriteString(str)
+		ret.WriteByte(newlineByte)
+
+		ret.Write(pubkeyHeader)
+		str = hex.EncodeToString(acc.PublicKey)
+		ret.WriteString(str)
+		ret.WriteByte(newlineByte)
+	}
+	return ret.String()
+}
+
+func (wlt *Wallet) Sign(idx int, msg ...[]byte) []byte {
+	msgHash := crypto.Sha512(append(sign_header_arr, msg...)...)
+	return eddsa.Sign(wlt.Acc[idx].PrivateKey, msgHash)
+}
+
+func (wlt *Wallet) SignHash(idx int, msgHash []byte) []byte {
+	return eddsa.Sign(wlt.Acc[idx].PrivateKey, msgHash)
+}
+
+
+func (wlt *Wallet) VerifyByRaw(idx int, signature []byte, msg ...[]byte) bool {
+	msgHash := crypto.Sha512(append(sign_header_arr, msg...)...)
+	return eddsa.Verify(wlt.Acc[idx].PublicKey, msgHash, signature)
+}
+
+func (wlt *Wallet) VerifyByHash(idx int, signature []byte, msgHash []byte) bool {
+	return eddsa.Verify(wlt.Acc[idx].PublicKey, msgHash, signature)
+}
+
+func (wlt *Wallet) Save() error {
+	bt, err := json.Marshal(wlt)
+	if err != nil {
+		return err
+	}
+	return wlt.db.Put([]byte(wlt.name), bt, nil)
 }
