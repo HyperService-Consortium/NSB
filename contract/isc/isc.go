34edfca276e90a2ad481228f362121afe4cb341b
Myriad-Dreamin <camiyoru@gmail.com>
2019-04-30 13:32:17 +0800

    Update isc.go

--- contract/isc/isc.go
+++ contract/isc/isc.go
@@ -42,7 +42,7 @@ type ArgsCreateNewContract struct {
 // // 0x637265617465495343197b226973635f6f776e657273223a5b22456a525765413d3d222c22456a5257654a6f3d225d2c2272657175697265645f66756e6473223a5b302c305d2c22566573536967223a22497a4d3d222c225472616e73616374696f6e496e74656e7473223a5b7b2266726f6d223a22456a525765413d3d222c22746f223a22456a5257654a6f3d222c22736571223a302c22616d74223a302c226d657461223a2249673d3d227d5d7d
 
 
-func (nsb *NSBApplication) CreateNewContract(byteJson []byte) (*cmn.ContractCallBackInfo) {
+func CreateNewContract(JsonParas []byte) (*cmn.ContractCallBackInfo) {
 	var args ArgsCreateNewContract
 	err := json.Unmarshal(JsonParas, args)
 	if err != nil {
