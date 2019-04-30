f310cdd50a41e5930c10a63708625648be931b69
Myriad-Dreamin <camiyoru@gmail.com>
2019-04-30 13:31:12 +0800

    Update isc.go

--- contract/isc/isc.go
+++ contract/isc/isc.go
@@ -1,8 +1,10 @@
 package isc
 
 import (
+	"fmt"
 	"encoding/json"
 	cmn "github.com/Myriad-Dreamin/NSB/common"
+	"github.com/Myriad-Dreamin/NSB/contract/isc/transaction"
 )
 
 type RequestCallISC struct {
@@ -31,26 +33,25 @@ func RigisteredMethod(env *cmn.ContractEnvironment) *cmn.ContractCallBackInfo {
 // 	}
 // }
 
-// type RequestCreateISC struct {
-// 	IscOwners          [][]byte                        `json:"isc_owners"`
-// 	Funds              []uint32                        `json:"required_funds"`
-// 	VesSig             []byte                          `json:ves_signature`
-// 	TransactionIntents []transaction.TransactionIntent `json: transactionIntents`
-// }
+type ArgsCreateNewContract struct {
+	IscOwners          [][]byte                        `json:"isc_owners"`
+	Funds              []uint32                        `json:"required_funds"`
+	VesSig             []byte                          `json:"ves_signature"`
+	TransactionIntents []transaction.TransactionIntent `json:"transactionIntents"`
+}
 // // 0x637265617465495343197b226973635f6f776e657273223a5b22456a525765413d3d222c22456a5257654a6f3d225d2c2272657175697265645f66756e6473223a5b302c305d2c22566573536967223a22497a4d3d222c225472616e73616374696f6e496e74656e7473223a5b7b2266726f6d223a22456a525765413d3d222c22746f223a22456a5257654a6f3d222c22736571223a302c22616d74223a302c226d657461223a2249673d3d227d5d7d
 
 
-// func (nsb *NSBApplication) createISC(byteJson []byte) (types.ResponseDeliverTx) {
-// 	var req RequestCreateISC
-// 	err := json.Unmarshal(byteJson, &req)
-// 	if err != nil {
-// 		return types.ResponseDeliverTx{
-// 			Code: uint32(CodeDecodeJsonError),
-// 		}
-// 	}
-// 	fmt.Print(req)
-// 	return types.ResponseDeliverTx{
-// 		Code: uint32(CodeOK),
-// 		Log: fmt.Sprintf("%v", req),
-// 	}
-// }
+func (nsb *NSBApplication) CreateNewContract(byteJson []byte) (*cmn.ContractCallBackInfo) {
+	var args ArgsCreateNewContract
+	err := json.Unmarshal(JsonParas, args)
+	if err != nil {
+		return DecodeJsonError(err)
+	}
+
+	fmt.Print(args)
+	return &cmn.ContractCallBackInfo{
+		Code: uint32(CodeOK),
+		Log: fmt.Sprintf("%v", args),
+	}
+}
