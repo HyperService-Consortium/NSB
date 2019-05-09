0759c6e533b3375bfe458b0f9947ca4e7d975399
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2019-05-10 00:46:12 +0800

    contract_invoke_helper

--- bin/py_nsbcli/modules/contract.py
+++ bin/py_nsbcli/modules/contract.py
@@ -3,6 +3,8 @@ from io import BytesIO
 from config import ENC
 from py_nsbcli import TransactionHeader, Wallet
 from py_nsbcli.modules import Client
+from py_nsbcli.util import GoJson
+
 
 
 class Contract(object):
@@ -10,8 +12,7 @@ class Contract(object):
         self.cli = cli
 
     def broadcast_tx_commit(self, tx: str):
-        response = self.cli.get_json(self.cli.admin.broadcast_tx_commit_url, params={"tx": tx})
-        print(response)
+        return self.cli.get_json(self.cli.admin.broadcast_tx_commit_url, params={"tx": tx})
 
     @staticmethod
     def generate_contract_tx(transaction_type: bytes, contract_type: bytes, tx_header: bytes):
@@ -44,6 +45,31 @@ class Contract(object):
             contract_name,
             tx_header.json().encode('utf-8')
         ))
+
+    def contract_invoke_helper(
+        self,
+        contract_name: str or bytes,
+        contract_address, function_name, args: list, value,
+        sig_or_wallet: Wallet or bytes,
+        fr=None, encoder_helper=None, default_helper=None
+    ):
+        data = GoJson.dumps({
+            "function_name": function_name,
+            "args": dict(((str(idx + 1), arg) for idx, arg in enumerate(args)))
+        }, cls=encoder_helper, default_helper=default_helper).encode(ENC)
+
+        if isinstance(sig_or_wallet, Wallet):
+            if fr is None:
+                fr = sig_or_wallet.address(0)
+
+            tx_header = TransactionHeader(fr, contract_address, data, value)
+            tx_header.sign(sig_or_wallet)
+        else:
+            tx_header = TransactionHeader(fr, contract_address, data, value, sig_or_wallet)
+        if fr is None:
+            raise ValueError("from address is necessary")
+
+        return self.exec_contract_method(contract_name, tx_header)
 
     def exec_system_contract_method(self, wlt: Wallet, prec_name: bytes, args: bytes, value: int):
 
