f520c8ff88cf661ecbf0ebcd927f5309e776363f
Myriad-Dreamin <camiyoru@gmail.com>
2019-05-10 17:39:38 +0800

    transfer

--- application/system.token.go
+++ application/system.token.go
@@ -10,10 +10,18 @@ import (
 /*
  * storage := actionMap
  */
+type ArgsSetBalance struct {
+	Value *math.Uint256 `json:"1"`
+}
+
+type ArgsTransfer struct {
+	Value *math.Uint256 `json:"1"`
+}
 
 func (nsb *NSBApplication) TokenRigisteredMethod(
 	env *cmn.TransactionHeader,
-	accInfo *AccountInfo,
+	frInfo *AccountInfo,
+	toInfo *AccountInfo,
 	funcName string,
 	args []byte,
 ) *types.ResponseDeliverTx {
@@ -21,18 +29,34 @@ func (nsb *NSBApplication) TokenRigisteredMethod(
 	case "setBalance":
 		var uargs ArgsSetBalance
 		MustUnmarshal(args, &uargs)
-		return nsb.setBalance(uargs.Value)
-	case "getAction":
-		return nsb.getAction(args)
+		return nsb.setBalance(frInfo, uargs.Value)
+	case "transfer":
+		var uargs ArgsTransfer
+		MustUnmarshal(args, &uargs)
+		return nsb.transfer(frInfo, toInfo, uargs.Value)
 	default:
 		return response.InvalidFuncTypeError(MethodMissing)
 	}
 }
 
-type ArgsSetBalance struct {
-	Value *math.Uint256 `json:"1"`
+func (nsb *NSBApplication) setBalance(accInfo *AccountInfo, value *math.Uint256) *types.ResponseDeliverTx {
+	accInfo.Balance = value
+	return response.ExecOK()
 }
 
-func (nsb *NSBApplication) setBalance(value *math.Uint256) *types.ResponseDeliverTx {
-	return response.UndateBalanceOut(value)
+
+func (nsb *NSBApplication) transfer(
+	frInfo *AccountInfo,
+	toInfo *AccountInfo,
+	value *math.Uint256
+) *types.ResponseDeliverTx {
+	checkErr := frInfo.Balance.Sub(value)
+	if checkErr {
+		return response.ExecContractError("'from' account has no enough token to substract")
+	}
+	checkErr := toInfo.Balance.Add(value)
+	if checkErr {
+		return response.ExecContractError("'to' account's balance overflow")
+	}
+	return response.ExecOK()
 }
