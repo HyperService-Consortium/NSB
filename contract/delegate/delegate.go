6c3e89445c871df56b1e65c78aebed48a7d83436
Zhuotao (Leo) Liu <zhuotaoliu@google.com>
2019-05-11 03:53:45 -0700

    delegate contract

---
+++ contract/delegate/delegate.go
@@ -0,0 +1,76 @@
+package delegate
+
+import (
+	"encoding/hex"
+	"fmt"
+	cmn "github.com/Myriad-Dreamin/NSB/common"
+	. "github.com/Myriad-Dreamin/NSB/common/contract_response"
+	"github.com/Myriad-Dreamin/NSB/math"
+)
+
+var count int = 0
+
+const NIN_PROPOSAL_COUNT = 5;
+
+type Delegate struct {
+	env *cmn.ContractEnvironment
+}
+
+func (delegate *Delegate) NewContract(_delegates [][]byte, district string, totalVotes *math.Uint256)(*cmn.ContractCallBackInfo){
+
+	delegate.env.Storage.NewBytesMap("isDelegate")
+	delegate.env.Storage.NewInt64Map("delegates")
+	for i,x:= range _delegates{
+		AssertFalse(len(x)==0,"delegate is not null" )
+		_=delegate.env.Storage.NewBytesMap("isDelegate").Set(x,[]byte("true"))
+		_=delegate.env.Storage.NewInt64Map("delegates").Set(int64(i),x)
+		count += 1
+	}
+
+	delegate.env.Storage.SetBytes("totalVotes", totalVotes.Bytes())
+	delegate.env.Storage.SetString("district", district)
+
+	return &cmn.ContractCallBackInfo{
+		CodeResponse: uint32(codeOK),
+		Info: fmt.Sprintf(
+			"create success , this contract is deploy at %v",
+			hex.EncodeToString(delegate.env.ContractAddress),
+		),
+	}
+}
+
+func (delegate *Delegate) Vote() (*cmn.ContractCallBackInfo){
+	cher,_:= delegate.env.Storage.NewBytesMap("isDelegate").Get(delegate.env.From)
+	AssertTrue(string(cher)=="true", "delegate Only")
+
+	str,_:= delegate.env.Storage.NewBytesMap("isDelegateVoted").Get(delegate.env.From)
+	if string(str)== "false"{
+		_= delegate.env.Storage.NewBytesMap("isDelegateVoted").Set(delegate.env.From,[]byte("true"))
+		totalVotes := math.NewUint256FromBytes(delegate.env.Storage.GetBytes("totalVotes"))
+		totalVotes.Add(math.NewUint256FromString("1", 10))
+		delegate.env.Storage.SetBytes("totalVotes", totalVotes.Bytes())
+	}
+
+	return &cmn.ContractCallBackInfo{
+		CodeResponse: uint32(codeOK),
+		Info: fmt.Sprintf(
+			"Total votes is now %v",
+			hex.EncodeToString(delegate.env.Storage.GetBytes("totalVotes")),
+		),
+	}
+}
+
+func (delegate *Delegate) ResetVote() (*cmn.ContractCallBackInfo){
+	cher,_:= delegate.env.Storage.NewBytesMap("isDelegate").Get(delegate.env.From)
+	AssertTrue(string(cher)=="true", "delegate Only")
+
+	_ = delegate.env.Storage.NewBytesMap("isDelegateVoted").Set(delegate.env.From,[]byte("false"))
+
+	return &cmn.ContractCallBackInfo{
+		CodeResponse: uint32(codeOK),
+		Info: fmt.Sprintf(
+			"delegate %v has been reset to false",
+			hex.EncodeToString(delegate.env.From),
+		),
+	}
+}
---
+++ contract/delegate/register.go
@@ -0,0 +1,51 @@
+package delegate
+
+import (
+	"encoding/json"
+	cmn "github.com/Myriad-Dreamin/NSB/common"
+	. "github.com/Myriad-Dreamin/NSB/common/contract_response"
+	"github.com/Myriad-Dreamin/NSB/math"
+)
+
+type ArgsCreateNewContract struct {
+	Delegates       [][]byte        `json:"1"`
+	District        string          `json:"2"`
+	TotalVotes      *math.Uint256   `json:"3"`
+}
+
+type ArgsAddDelegate struct {
+	NewDelegate	[]byte			`json:"1"`
+}
+
+type ArgsRemoveDelegate struct {
+	RemovedDelegate	  []byte		`json:"1"`
+}
+
+func MustUnmarshal(data []byte, load interface{}) {
+	err := json.Unmarshal(data, &load)
+	if err != nil {
+		panic(DecodeJsonError(err))
+	}
+}
+
+func RigisteredMethod(contractEnvironment *cmn.ContractEnvironment) *cmn.ContractCallBackInfo {
+	var delegate = &Delegate{env: contractEnvironment}
+	switch contractEnvironment.FuncName {
+	case "Vote":
+		return delegate.Vote()
+	case "ResetVote":
+		return delegate.ResetVote()
+	default:
+		return InvalidFunctionType(contractEnvironment.FuncName)
+	}
+}
+
+
+func CreateNewContract(contractEnvironment *cmn.ContractEnvironment) (*cmn.ContractCallBackInfo) {
+	var args ArgsCreateNewContract
+	MustUnmarshal(contractEnvironment.Args, &args)
+
+	var delegate = &Delegate{env: contractEnvironment}
+	return delegate.NewContract(args.Delegates, args.District, args.TotalVotes)
+}
+
