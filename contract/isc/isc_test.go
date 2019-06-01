a7ba1faa9f8f4ba055986c3a7d82f4177b0678e3
Myriad-Dreamin <camiyoru@gmail.com>
2019-06-01 10:01:39 +0800

    isc init-open

--- contract/isc/isc_test.go
+++ contract/isc/isc_test.go
@@ -5,8 +5,10 @@ import (
 	"github.com/HyperServiceOne/NSB/math"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/HyperServiceOne/NSB/contract/isc/transaction"
+	"github.com/HyperServiceOne/NSB/contract/isc/ISCState"
 	cmn "github.com/HyperServiceOne/NSB/common"
 	"fmt"
+	"bytes"
 	"testing"
 	"encoding/json"
 )
@@ -68,6 +70,7 @@ type ContractEnvironment struct {
 func TestISCMakeISC(t *testing.T) {
 	defer func() {
 		if err := recover(); err != nil {
+			__x_storage.Revert()
 			t.Error(err)
 			return
 		}
@@ -102,11 +105,13 @@ func TestISCMakeISC(t *testing.T) {
 	__x_env.ContractAddress = []byte{0, 1, 2, 3}
 	__x_env.Args = bt
 	fmt.Println(CreateNewContract(__x_env))
+	__x_storage.Commit()
 }
 
 func TestISCBadMakeISC(t *testing.T) {
 	defer func() {
 		if err := recover(); err != nil {
+			__x_storage.Revert()
 			fmt.Println(err)
 			return
 		}
@@ -145,6 +150,7 @@ func TestISCBadMakeISC(t *testing.T) {
 func TestISCBadMakeISC2(t *testing.T) {
 	defer func() {
 		if err := recover(); err != nil {
+			__x_storage.Revert()
 			fmt.Println(err)
 			return
 		}
@@ -180,6 +186,173 @@ func TestISCBadMakeISC2(t *testing.T) {
 	fmt.Println(CreateNewContract(__x_env))
 }
 
+func TestResetInfo(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			__x_storage.Revert()
+			fmt.Println(err)
+			return
+		}
+	}()
+	
+
+	var u, v = []byte{0, 0, 1}, []byte{0, 0, 2};
+	var transactionIntent = &transaction.TransactionIntent{
+		Fr: u,
+		To: v,
+		Seq: math.NewUint256FromHexString("666"),
+		Amt: math.NewUint256FromHexString("666"),
+		Meta: []byte{0},
+	}
+	var args = &ArgsUpdateTxInfo {
+		Tid: 0,
+		TransactionIntent: transactionIntent,
+	}
+	bt, err := json.Marshal(args)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	__x_env.From = u
+	__x_env.ContractAddress = []byte{0, 1, 2, 3}
+	__x_env.FuncName = "UpdateTxInfo"
+	__x_env.Args = bt
+	txArr := __x_storage.NewBytesArray("transactions")
+	fmt.Println("set", txArr.Get(0))
+
+	fmt.Println(RigisteredMethod(__x_env))
+	__x_storage.Commit()
+	fmt.Println("set", txArr.Get(0))
+
+	bt, err = json.Marshal(transactionIntent)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if ! bytes.Equal(bt, txArr.Get(0)) {
+		fmt.Println("Test Set Info not equal...")
+	}
+}
+
+func TestFreezeInfo(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			__x_storage.Revert()
+			fmt.Println(err)
+			return
+		}
+	}()
+	
+
+	var u = []byte{0, 0, 1}
+	var args = &ArgsFreezeInfo {
+		Tid: 0,
+	}
+	bt, err := json.Marshal(args)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	__x_env.From = u
+	__x_env.ContractAddress = []byte{0, 1, 2, 3}
+	__x_env.FuncName = "FreezeInfo"
+	__x_env.Args = bt
+
+	fmt.Println(RigisteredMethod(__x_env))
+	__x_storage.Commit()
+
+	if __x_storage.NewUint64Map("transactionsFrozen").Get(0) == nil {
+		fmt.Println("not frozen")
+	}
+	fmt.Println(__x_storage.GetUint8("iscState") == ISCState.Inited)
+}
+
+func TestResetInfoAfterAllFrozen(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			__x_storage.Revert()
+			fmt.Println(err)
+			return
+		}
+	}()
+	
+
+	var u, v = []byte{0, 0, 1}, []byte{0, 0, 2};
+	var transactionIntent = &transaction.TransactionIntent{
+		Fr: u,
+		To: v,
+		Seq: math.NewUint256FromHexString("666"),
+		Amt: math.NewUint256FromHexString("666"),
+		Meta: []byte{0},
+	}
+	var args = &ArgsUpdateTxInfo {
+		Tid: 0,
+		TransactionIntent: transactionIntent,
+	}
+	bt, err := json.Marshal(args)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	__x_env.From = u
+	__x_env.ContractAddress = []byte{0, 1, 2, 3}
+	__x_env.FuncName = "UpdateTxInfo"
+	__x_env.Args = bt
+	txArr := __x_storage.NewBytesArray("transactions")
+	fmt.Println("set", txArr.Get(0))
+
+	fmt.Println(RigisteredMethod(__x_env))
+	__x_storage.Commit()
+	fmt.Println("set", txArr.Get(0))
+
+	bt, err = json.Marshal(transactionIntent)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if ! bytes.Equal(bt, txArr.Get(0)) {
+		fmt.Println("Test Set Info not equal...")
+	}
+}
+
+
+func TestUserAcks(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			__x_storage.Revert()
+			fmt.Println(err)
+			return
+		}
+	}()
+	
+
+	var u, v = []byte{0, 0, 1}, []byte{0, 0, 2}
+	var args = &ArgsUserAck {
+		Signature: []byte("test..."),
+	}
+	bt, err := json.Marshal(args)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	__x_env.From = u
+	__x_env.ContractAddress = []byte{0, 1, 2, 3}
+	__x_env.FuncName = "UserAck"
+	__x_env.Args = bt
+
+	fmt.Println(RigisteredMethod(__x_env))
+	__x_storage.Commit()
+	
+	
+	__x_env.From = v
+
+	fmt.Println(RigisteredMethod(__x_env))
+	__x_storage.Commit()
+	
+	fmt.Println(__x_storage.GetUint8("iscState") == ISCState.Opening)
+}
 
 func TestUnmakeStorage(t *testing.T) {
 	__x_ldb.Close()
--- contract/isc/register.go
+++ contract/isc/register.go
@@ -22,8 +22,27 @@ type ArgsCreateNewContract struct {
 	IscOwners          [][]byte                        `json:"isc_owners"`
 	Funds              []uint32                        `json:"required_funds"`
 	VesSig             []byte                          `json:"ves_signature"`
-	TransactionIntents []*transaction.TransactionIntent `json:"transactionIntents"`
+	TransactionIntents []*transaction.TransactionIntent `json:"transaction_intents"`
 }
+
+type ArgsUpdateTxInfo struct {
+	Tid uint64 `json:"tid"`
+	TransactionIntent *transaction.TransactionIntent `json:"transaction_intent"`
+}
+
+type ArgsUpdateTxFr struct {
+	Tid uint64 `json:"tid"`
+	Fr []byte `json:"from"`
+}
+
+type ArgsFreezeInfo struct {
+	Tid uint64 `json:"tid"`
+}
+
+type ArgsUserAck struct {
+	Signature []byte `json:"signature"`
+}
+
 
 func CreateNewContract(contractEnvironment *cmn.ContractEnvironment) (*cmn.ContractCallBackInfo) {
 	var args ArgsCreateNewContract
@@ -34,9 +53,24 @@ func CreateNewContract(contractEnvironment *cmn.ContractEnvironment) (*cmn.Contr
 }
 
 func RigisteredMethod(env *cmn.ContractEnvironment) *cmn.ContractCallBackInfo {
+	var iscc = &ISC{env: env}
 	switch env.FuncName {
-	case "a+b":
-		return SafeAdd(env.Args)
+	case "UpdateTxInfo":
+		var args ArgsUpdateTxInfo
+		MustUnmarshal(env.Args, &args)
+		return iscc.UpdateTxInfo(args.Tid, args.TransactionIntent)
+	case "UpdateTxFr":
+		var args ArgsUpdateTxFr
+		MustUnmarshal(env.Args, &args)
+		return iscc.UpdateTxFr(args.Tid, args.Fr)
+	case "FreezeInfo":
+		var args ArgsFreezeInfo
+		MustUnmarshal(env.Args, &args)
+		return iscc.FreezeInfo(args.Tid)
+	case "UserAck":
+		var args ArgsUserAck
+		MustUnmarshal(env.Args, &args)
+		return iscc.UserAck(args.Signature)
 	default:
 		return InvalidFunctionType(env.FuncName)
 	}
