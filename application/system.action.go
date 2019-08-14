19b17988dc5470a46e8c93df32c8b13b7f37dee1
Myriad-Dreamin <camiyoru@gmail.com>
2019-08-15 05:52:51 +0800

    add actions

--- application/system.action.go
+++ application/system.action.go
@@ -2,11 +2,12 @@ package nsb
 
 import (
 	"encoding/json"
+
+	autil "github.com/HyperServiceOne/NSB/action"
 	"github.com/HyperServiceOne/NSB/application/response"
 	cmn "github.com/HyperServiceOne/NSB/common"
 	"github.com/HyperServiceOne/NSB/crypto"
 	"github.com/HyperServiceOne/NSB/util"
-	autil "github.com/HyperServiceOne/NSB/action"
 	"github.com/tendermint/tendermint/abci/types"
 )
 
@@ -21,6 +22,19 @@ func MustUnmarshal(data []byte, load interface{}) {
 	}
 }
 
+type ArgsAddAction struct {
+	ISCAddress []byte `json:"1"`
+	Tid        uint64 `json:"2"`
+	Aid        uint64 `json:"3"`
+	Type       uint8  `json:"4"`
+	Content    []byte `json:"5"`
+	Signature  []byte `json:"6"`
+}
+
+type ArgsAddActions struct {
+	Args []ArgsAddAction `json:"1"`
+}
+
 func (nsb *NSBApplication) ActionRigisteredMethod(
 	env *cmn.TransactionHeader,
 	frInfo *AccountInfo,
@@ -30,41 +44,57 @@ func (nsb *NSBApplication) ActionRigisteredMethod(
 ) *types.ResponseDeliverTx {
 	switch funcName {
 	case "addAction":
-		return nsb.addAction(args)
+		var argsAddAction ArgsAddAction
+		MustUnmarshal(args, &argsAddAction)
+		return nsb.addAction(&argsAddAction)
 	case "getAction":
 		return nsb.getAction(args)
+	case "addActions":
+		var argsAddActions ArgsAddActions
+		MustUnmarshal(args, &argsAddActions)
+		return nsb.addActions(&argsAddActions)
 	default:
 		return response.InvalidFuncTypeError(MethodMissing)
 	}
-}
-
-type ArgsAddAction struct {
-	ISCAddress []byte `json:"1"`
-	Tid uint64 `json:"2"`
-	Aid       uint64 `json:"3"`
-	Type      uint8  `json:"4"`
-	Content   []byte `json:"5"`
-	Signature []byte `json:"6"`
 }
 
 func actionKey(addr []byte, tid uint64, aid uint64) []byte {
 	return crypto.Sha512(addr, util.Uint64ToBytes(tid), util.Uint64ToBytes(aid))
 }
 
-func (nsb *NSBApplication) addAction(bytesArgs []byte) *types.ResponseDeliverTx {
-	var args ArgsAddAction
-	MustUnmarshal(bytesArgs, &args)
+func (nsb *NSBApplication) _addAction(args *ArgsAddAction) error {
 	// TODO: check valid isc/tid/aid
 	action, err := autil.NewAction(args.Type, args.Signature, args.Content)
 	if err != nil {
-		return response.ExecContractError(err)
+		return err
 	}
 	err = nsb.actionMap.TryUpdate(
 		actionKey(args.ISCAddress, args.Tid, args.Aid),
 		action.Concat(),
 	)
 	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (nsb *NSBApplication) addAction(args *ArgsAddAction) *types.ResponseDeliverTx {
+
+	if err := nsb._addAction(args); err != nil {
 		return response.ExecContractError(err)
+	}
+
+	return &types.ResponseDeliverTx{
+		Code: uint32(response.CodeOK()),
+		Info: "updateSuccess",
+	}
+}
+
+func (nsb *NSBApplication) addActions(args *ArgsAddActions) *types.ResponseDeliverTx {
+	for _, batchArgs := range args.Args {
+		if err := nsb._addAction(&batchArgs); err != nil {
+			return response.ExecContractError(err)
+		}
 	}
 	return &types.ResponseDeliverTx{
 		Code: uint32(response.CodeOK()),
