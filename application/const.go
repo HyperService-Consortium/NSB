10302bf15b7331c14d044e25dfa5bd37c1375716
Myriad-Dreamin <camiyoru@gmail.com>
2019-08-20 08:36:46 +0800

    fix info & set const

--- application/const.go
+++ application/const.go
@@ -2,16 +2,24 @@ package nsb
 
 import (
 	"errors"
+
 	"github.com/tendermint/tendermint/version"
 )
 
 const (
-	NSBVersion version.Protocol = 0x1
+	NSBVersion version.Protocol = 0x2
 )
 
+type slot = []byte
+
 var (
-	stateKey     = []byte("NSBStateKey")
-	actionHeader = []byte("NACHD:")
+	stateKey                       = []byte("NSBStateKey")
+	actionHeader                   = []byte("NACHD:")
+	accMapSlot                     = slot("acc:")
+	txMapSlot                      = slot("tx:")
+	actionMapSlot                  = slot("act:")
+	validMerkleProofMapSlot        = slot("vlm:")
+	validOnchainMerkleProofMapSlot = slot("vom:")
 )
 
 var (
