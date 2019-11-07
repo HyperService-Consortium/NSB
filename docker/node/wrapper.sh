5ed4712f921837993c1a6106b120edd0175a06c8
Myriad-Dreamin <camiyoru@gmail.com>
2019-11-07 23:00:47 +0800

    hf

--- docker/node/wrapper.sh
+++ docker/node/wrapper.sh
@@ -3,27 +3,27 @@
 ##
 ## Input parameters
 ##
-BINARY=/usr/bin/tendermint
+TENDERMINT_BINARY=/tendermint/tendermint
+NSB_BINARY=/usr/bin/NSB
 ID=${ID:-0}
 LOG=${LOG:-tendermint.log}
 
 echo current ID=${ID}
 echo current LOG=${LOG}
 
-# cp /usr/bin/NSB /usr/bin/NSB${ID}
-if [ ! -f "/usr/bin/NSB" ]; then
-	echo "file /usr/bin/NSB not found"
+if [ ! -f "${NSB_BINARY}" ]; then
+	echo "file ${NSB_BINARY} not found"
 	exit 1
 fi
-nohup /usr/bin/NSB -db=$DB_DIR -server=$TCP_AD -port=$PORT &
+nohup ${NSB_BINARY} -db=$DB_DIR -server=$TCP_AD -port=$PORT &
 ##
 ## Assert linux binary
 ##
-if ! [ -f "${BINARY}" ]; then
-	echo "The binary $(basename "${BINARY}") cannot be found. Please add the binary to the shared folder. Please use the BINARY environment variable if the name of the binary is not 'tendermint' E.g.: -e BINARY=tendermint_my_test_version"
+if ! [ -f "${TENDERMINT_BINARY}" ]; then
+	echo "The binary $(basename "${TENDERMINT_BINARY}") cannot be found. Please add the binary to the shared folder. Please use the TENDERMINT_BINARY environment variable if the name of the binary is not 'tendermint' E.g.: -e TENDERMINT_BINARY=tendermint_my_test_version"
 	exit 1
 fi
-BINARY_CHECK="$(file "$BINARY" | grep 'ELF 64-bit LSB executable, x86-64')"
+BINARY_CHECK="$(file "$TENDERMINT_BINARY" | grep 'ELF 64-bit LSB executable, x86-64')"
 if [ -z "${BINARY_CHECK}" ]; then
 	echo "Binary needs to be OS linux, ARCH amd64"
 	exit 1
@@ -35,11 +35,11 @@ fi
 export TMHOME="/tendermint/node${ID}"
 
 if [ -d "`dirname ${TMHOME}/${LOG}`" ]; then
-  echo running "$BINARY" "$@" | tee "${TMHOME}/${LOG}"
-  "$BINARY" "$@" | tee "${TMHOME}/${LOG}"
+  echo running "$TENDERMINT_BINARY" "$@" | tee "${TMHOME}/${LOG}"
+  "$TENDERMINT_BINARY" "$@" | tee "${TMHOME}/${LOG}"
 else
-  echo running "$BINARY" "$@"
-  "$BINARY" "$@"
+  echo running "$TENDERMINT_BINARY" "$@"
+  "$TENDERMINT_BINARY" "$@"
 fi
 
 chmod 777 -R /tendermint
